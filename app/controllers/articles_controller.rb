505ab97561c0b322c1822b9b4a197831797fac91
Kamlesh Haswani <kamlesh@infotech.works>
2018-11-04 16:03:49 +0530

    Updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -12,6 +16,19 @@ class ArticlesController < ApplicationController
         redirect_to article_path(@article)
       else
         render 'new'
+      end
+    #redirect_to article_path(@article)
+  end
+
+  def update
+    #render plain: params[:article].inspect
+    @article = Article.find(params[:id])
+    #@article.save
+    if @article.update(article_params)
+        flash[:notice] = "Article udpated successfully"
+        redirect_to article_path(@article)
+      else
+        render 'edit'
       end
     #redirect_to article_path(@article)
   end
